130087479e88614eb7873dc19d21737dad7bad2c
Juancho022 <juanma.francesconi15@gmail.com>
2024-05-10 15:55:40 -0300

    Agregando json de hoteles

--- src/main/java/com/example/AgenciaTurismo/AgenciaTurismoApplication.java
+++ src/main/java/com/example/AgenciaTurismo/AgenciaTurismoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AgenciaTurismoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AgenciaTurismoApplication.class, args);
+
+
 	}
 
 }
