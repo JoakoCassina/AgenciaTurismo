0d782b096a42b63c7b9c9fd52d3d5e21e3f23303
Agustina Peralta <xagustinaperalta@gmail.com>
2024-05-13 14:06:28 -0300

    agregando controller historia de usuario 1

--- src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
@@ -1,8 +1,25 @@
 package com.example.AgenciaTurismo.controller;
 
+import com.example.AgenciaTurismo.service.IHotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HotelController {
+
+    @Autowired
+    private IHotelService hotelService;
+
+    @GetMapping("/listarHoteles") //
+    public ResponseEntity<?> listarHoteles(){
+        return new ResponseEntity<>(hotelService.listHotelsDTO(), HttpStatus.OK);
+    }
+
+
+
+
 
 }
