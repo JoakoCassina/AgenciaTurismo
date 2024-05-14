c1cb0b4a08a23699d18243bfe6ec8c039a6aed15
Joaquin Cassina <jcassina@ams.red>
2024-05-14 14:29:48 -0300

    US005

--- src/main/java/com/example/AgenciaTurismo/controller/FlightController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/FlightController.java
@@ -1,14 +1,22 @@
 package com.example.AgenciaTurismo.controller;
 
+import com.example.AgenciaTurismo.dto.request.FlightConsultDTO;
+import com.example.AgenciaTurismo.dto.response.FlightAvailableDTO;
 import com.example.AgenciaTurismo.service.IFlightService;
 import com.example.AgenciaTurismo.service.IHotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
+@RequestMapping("/api/v1")
 public class FlightController {
 
     @Autowired
@@ -18,5 +26,16 @@ public class FlightController {
     public ResponseEntity<?> listFlightsDTO(){
         return new ResponseEntity<>(flightService.listFlightsDTO(), HttpStatus.OK);
     }
+
+    @GetMapping("/flights")
+    public ResponseEntity<?> vuelosDisponibles(@DateTimeFormat(pattern = "dd-MM-yyyy") @RequestParam LocalDate dateFrom,
+                                               @DateTimeFormat(pattern = "dd-MM-yyyy") @RequestParam LocalDate dateTo,
+                                               @RequestParam String origin,
+                                               @RequestParam String destination){
+        FlightConsultDTO datos = new FlightConsultDTO(dateFrom, dateTo, origin, destination);
+        return new ResponseEntity<>(flightService.vuelosDisponibles(datos), HttpStatus.OK);
+    }
+
+
 
 }
--- src/main/java/com/example/AgenciaTurismo/service/FlightService.java
+++ src/main/java/com/example/AgenciaTurismo/service/FlightService.java
@@ -6,10 +6,12 @@ import com.example.AgenciaTurismo.dto.request.FinalReservationDTO;
 import com.example.AgenciaTurismo.dto.request.FlightConsultDTO;
 import com.example.AgenciaTurismo.dto.response.FlightAvailableDTO;
 import com.example.AgenciaTurismo.dto.response.TotalReservationDTO;
+import com.example.AgenciaTurismo.model.Flight;
 import com.example.AgenciaTurismo.repository.IFlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -34,7 +36,21 @@ public class FlightService implements IFlightService{
 
     @Override
     public FlightAvailableDTO vuelosDisponibles(FlightConsultDTO flightConsultDTO) {
-        return null;
+        List<FlightDTO> listFlightDTO = listFlightsDTO();
+
+        List<FlightDTO> availableFlight = new ArrayList<>();
+        for (FlightDTO flight : listFlightDTO) {
+            if (flight.getOrigin().equals(flightConsultDTO.getOrigin())
+                    && flight.getDestination().equals(flightConsultDTO.getDestination())
+                    && flight.getDateFrom().equals(flightConsultDTO.getDateFrom())
+                    && flight.getDateTo().equals(flightConsultDTO.getDateTo())) {
+                availableFlight.add(flight);
+            }
+        }
+        FlightAvailableDTO flightAvailable = new FlightAvailableDTO();
+        flightAvailable.setAvailableFlightDTO(availableFlight);
+
+        return flightAvailable;
     }
 
     @Override
