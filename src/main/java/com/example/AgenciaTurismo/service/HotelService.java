345cfe2b6a1f87a0cd10bc3d5a4f226c83809fe0
Andrea Toledo <atoledo618@gmail.com>
2024-05-14 15:17:51 -0300

    :sparkles:US 002

--- src/main/java/com/example/AgenciaTurismo/service/HotelService.java
+++ src/main/java/com/example/AgenciaTurismo/service/HotelService.java
@@ -1,11 +1,16 @@
 package com.example.AgenciaTurismo.service;
 
+import com.example.AgenciaTurismo.dto.FlightDTO;
+import com.example.AgenciaTurismo.dto.request.HotelConsultDTO;
+import com.example.AgenciaTurismo.dto.response.FlightAvailableDTO;
+import com.example.AgenciaTurismo.dto.response.HotelAvailableDTO;
 import com.example.AgenciaTurismo.model.Hotel;
 import com.example.AgenciaTurismo.repository.IHotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.AgenciaTurismo.dto.HotelDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,5 +30,23 @@ public class HotelService implements IHotelService{
                         hotel.getDateTo(),
                         hotel.getReserved()
                 )).toList();
+    }
+
+    @Override
+    public HotelAvailableDTO hotelesDisponibles(HotelConsultDTO hotelConsultDTO) {
+        List<HotelDTO> listHotelDTO = listHotelsDTO();
+
+        List<HotelDTO> availableHotel = new ArrayList<>();
+        for (HotelDTO hotel : listHotelDTO) {
+            if (hotel.getDestination().equals(hotelConsultDTO.getDestination())
+                    && hotel.getDateFrom().equals(hotelConsultDTO.getDateFrom())
+                    && hotel.getDateTo().equals(hotelConsultDTO.getDateTo())){
+                  availableHotel.add(hotel);
+            }
+        }
+        HotelAvailableDTO hotelAvailable = new HotelAvailableDTO();
+        hotelAvailable.setAvailableHotelDTO(availableHotel);
+
+        return hotelAvailable;
     }
 }
