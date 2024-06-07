4cb7e67b2bd09f77819f8c621a2e9be8da260c16
PeraltaAgus <agustina.peralta@sancorsalud.com.ar>
2024-06-07 16:09:12 -0300

    Creando metodo de integracion de historia de usuario 5

--- src/test/java/com/example/AgenciaTurismo/integrationTest/FlightControllerTest.java
+++ src/test/java/com/example/AgenciaTurismo/integrationTest/FlightControllerTest.java
@@ -1,32 +1,28 @@
 package com.example.AgenciaTurismo.integrationTest;
 
 import com.example.AgenciaTurismo.dto.FlightDTO;
-import com.example.AgenciaTurismo.dto.request.FlightConsultDTO;
-import com.example.AgenciaTurismo.dto.response.FlightAvailableDTO;
-import com.example.AgenciaTurismo.dto.response.HotelAvailableDTO;
+import com.example.AgenciaTurismo.dto.FlightReservationDTO;
+import com.example.AgenciaTurismo.dto.PaymentMethodDTO;
+import com.example.AgenciaTurismo.dto.PeopleDTO;
+import com.example.AgenciaTurismo.dto.request.FinalFlightReservationDTO;
+import com.example.AgenciaTurismo.dto.response.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
@@ -46,10 +42,22 @@ public class FlightControllerTest {
     private static final FlightDTO flightDTO2 = new FlightDTO("PIBA-1420", "Puerto Iguazú", "Bogotá", "Business", 43200, LocalDate.of(2025, 2, 10), LocalDate.of(2025, 2, 20));
     private static final FlightDTO flightDTO3 = new FlightDTO("PIBA-1420", "Puerto Iguazú", "Bogotá", "Economy", 25735, LocalDate.of(2025, 2, 10), LocalDate.of(2025, 2, 20));
 
-    /*@GetMapping("/listFlights") //
-    public ResponseEntity<?> listFlightsDTO(){
-        return new ResponseEntity<>(flightService.listFlightsDTO(), HttpStatus.OK);
-    }*/
+    //Personas para la lista enviada
+    private static final PeopleDTO peopleDTO1 = new PeopleDTO(42533885,"Joako","Cassina",LocalDate.of(2000,04,18), "joako@gmail.com");
+
+    private static final PeopleDTO peopleDTO2 = new PeopleDTO(420000,"Juan","Casi",LocalDate.of(1999,05,19), "juan@gmail.com");
+
+    //Metodo de pago enviado
+    private static final PaymentMethodDTO metodoDTO = new PaymentMethodDTO("Credit", "0001", 3);
+
+    private static final FlightReservationDTO reserva = new FlightReservationDTO(LocalDate.of(2025, 2, 10),
+            LocalDate.of(2025, 2, 15), "Buenos Aires", "Puerto Iguazú", "Andre1235", 2.0, "Economy",
+            List.of(peopleDTO1, peopleDTO2), metodoDTO);
+
+    // StatusCode de respuesta
+    private static final StatusCodeDTO statusCodeDTO = new StatusCodeDTO(201, "El proceso terminó satisfactoriamente");
+
+    //US 04: Listar vuelos disponibles
     @Test
     public void listarVuelosTestOK() throws Exception {
         ResultMatcher statusEsperado = MockMvcResultMatchers.status().isOk();
@@ -61,15 +69,6 @@ public class FlightControllerTest {
     }
 
     //US 05: buscar hoteles disponibles
-    /*@GetMapping("/flights")
-    public ResponseEntity<?> vuelosDisponibles(@RequestParam @Future(message = "La fecha de entrada debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy")  LocalDate dateFrom,
-                                               @RequestParam @Future(message = "La fecha de salida debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateTo,
-                                               @RequestParam @NotBlank String origin,
-                                               @RequestParam @NotBlank String destination){
-        FlightConsultDTO datos = new FlightConsultDTO(dateFrom, dateTo, origin, destination);
-        return new ResponseEntity<>(flightService.vuelosDisponibles(datos), HttpStatus.OK);
-    }*/
-
     @Test
     public void flightDisponiblesTestOK() throws Exception {
         String dateFromConsul = "10-02-2025";
@@ -93,5 +92,23 @@ public class FlightControllerTest {
                 .andDo(print());
     }
 
+    //us 06: devolver reserva hotel
+    @Test
+    public void reservedTestOK() throws Exception {
+        FinalFlightReservationDTO reservaConsulta = new FinalFlightReservationDTO("Agustina",reserva);
+        String payloadDto = objectMapper.writeValueAsString(reservaConsulta);
+
+        TotalFlightReservationDTO resultadoEsperado = new TotalFlightReservationDTO(13000.0, 650.0, 13650.0, reservaConsulta, statusCodeDTO);
+
+        ResultMatcher statusEsperado = MockMvcResultMatchers.status().isOk();
+        ResultMatcher bodyEsperado= MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(resultadoEsperado));
+        ResultMatcher contentTypeEsperado = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(post("/api/v1/flight-reservation")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payloadDto))
+                .andExpectAll(statusEsperado,bodyEsperado,contentTypeEsperado)
+                .andDo(print());
+    }
 
 }
