7d5925deee60053d2add95904b74eb684b2952f1
Joaquin Cassina <jcassina@ams.red>
2024-07-04 14:19:17 -0300

    problemas con los controller

--- src/main/java/com/example/AgenciaTurismo/controller/FlightController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/FlightController.java
@@ -18,58 +18,58 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@RestController
-@RequestMapping("/api/v1")
-public class FlightController {
-
-    @Autowired
-    private IFlightService flightService;
-
-    //US 0004:
-    @GetMapping("/listFlights") //
-    public ResponseEntity<?> listFlightsDTO(){
-        return new ResponseEntity<>(flightService.listFlightsDTO(), HttpStatus.OK);
-    }
-
-    //US 0005:
-    @GetMapping("/flights")
-    public ResponseEntity<?> vuelosDisponibles(@RequestParam @Future(message = "La fecha de entrada debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy")  LocalDate dateFrom,
-                                               @RequestParam @Future(message = "La fecha de salida debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateTo,
-                                               @RequestParam @NotBlank String origin,
-                                               @RequestParam @NotBlank String destination){
-        FlightConsultDTO datos = new FlightConsultDTO(dateFrom, dateTo, origin, destination);
-        return new ResponseEntity<>(flightService.vuelosDisponibles(datos), HttpStatus.OK);
-    }
-
-    //US 0006
-    @PostMapping("/flight-reservation")
-    public ResponseEntity<?> reserveFlight(@RequestBody @Valid FinalFlightReservationDTO finalFlightReservationDTO) {
-        return new ResponseEntity<>(flightService.reserved(finalFlightReservationDTO), HttpStatus.CREATED);
-    }
-    //VUELOS RESERVADOS
-    @GetMapping("/reservedFlights")
-    public ResponseEntity<?> flightsSaved() {
-        return new ResponseEntity<>(flightService.flightSaved(), HttpStatus.OK);
-    }
-
-    //CREATE
-    @PostMapping("/createFlight")
-    public ResponseEntity<ResponseDTO> createFlight(@RequestBody @Valid FlightDTO flightDTO) {
-
-        return new ResponseEntity<>(flightService.createFlight(flightDTO), HttpStatus.CREATED);
-    }
-
-    //UPDATE
-    @PutMapping("/updateFlight/{flightCode}")
-    public ResponseEntity<ResponseDTO> updateFlight(@PathVariable @NotBlank Long id, @RequestBody FlightDTO flightDTO) {
-
-        return new ResponseEntity<>(flightService.updateFlight(id, flightDTO), HttpStatus.OK);
-    }
-
-    //DELETE
-    @DeleteMapping("/deleteFlight/{flightCode}")
-    public ResponseEntity<ResponseDTO> deleteFlight(@PathVariable @NotBlank String flightCode) {
-        return new ResponseEntity<>(flightService.deleteFlight(flightCode), HttpStatus.OK);
-    }
-
-}
+//@RestController
+//@RequestMapping("/api/v1")
+//public class FlightController {
+//
+//    @Autowired
+//    private IFlightService flightService;
+//
+//    //US 0004:
+//    @GetMapping("/listFlights") //
+//    public ResponseEntity<?> listFlightsDTO(){
+//        return new ResponseEntity<>(flightService.listFlightsDTO(), HttpStatus.OK);
+//    }
+//
+//    //US 0005:
+//    @GetMapping("/flights")
+//    public ResponseEntity<?> vuelosDisponibles(@RequestParam @Future(message = "La fecha de entrada debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy")  LocalDate dateFrom,
+//                                               @RequestParam @Future(message = "La fecha de salida debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateTo,
+//                                               @RequestParam @NotBlank String origin,
+//                                               @RequestParam @NotBlank String destination){
+//        FlightConsultDTO datos = new FlightConsultDTO(dateFrom, dateTo, origin, destination);
+//        return new ResponseEntity<>(flightService.vuelosDisponibles(datos), HttpStatus.OK);
+//    }
+//
+//    //US 0006
+//    @PostMapping("/flight-reservation")
+//    public ResponseEntity<?> reserveFlight(@RequestBody @Valid FinalFlightReservationDTO finalFlightReservationDTO) {
+//        return new ResponseEntity<>(flightService.reserved(finalFlightReservationDTO), HttpStatus.CREATED);
+//    }
+//    //VUELOS RESERVADOS
+//    @GetMapping("/reservedFlights")
+//    public ResponseEntity<?> flightsSaved() {
+//        return new ResponseEntity<>(flightService.flightSaved(), HttpStatus.OK);
+//    }
+//
+//    //CREATE
+//    @PostMapping("/createFlight")
+//    public ResponseEntity<ResponseDTO> createFlight(@RequestBody @Valid FlightDTO flightDTO) {
+//
+//        return new ResponseEntity<>(flightService.createFlight(flightDTO), HttpStatus.CREATED);
+//    }
+//
+//    //UPDATE
+//    @PutMapping("/updateFlight/{flightCode}")
+//    public ResponseEntity<ResponseDTO> updateFlight(@PathVariable @NotBlank Long id, @RequestBody FlightDTO flightDTO) {
+//
+//        return new ResponseEntity<>(flightService.updateFlight(id, flightDTO), HttpStatus.OK);
+//    }
+//
+//    //DELETE
+//    @DeleteMapping("/deleteFlight/{flightCode}")
+//    public ResponseEntity<ResponseDTO> deleteFlight(@PathVariable @NotBlank String flightCode) {
+//        return new ResponseEntity<>(flightService.deleteFlight(flightCode), HttpStatus.OK);
+//    }
+//
+//}
--- src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
@@ -19,58 +19,58 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@RestController
-@RequestMapping("/api/v1")
-@Validated
-public class HotelController {
-
-    @Autowired
-    private IHotelService hotelService;
-
-    //US 0001:
-    @GetMapping("/listHotels") //
-    public ResponseEntity<?> listarHoteles(){
-        return new ResponseEntity<>(hotelService.listHotelsDTO(), HttpStatus.OK);
-    }
-
-    //US 0002:
-    @GetMapping("/hotels")
-    public ResponseEntity<?> hotelesDisponibles(@RequestParam  @Future(message = "La fecha de entrada debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateFrom,
-                                                @RequestParam @Future(message = "La fecha de salida debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateTo,
-                                               @RequestParam @NotBlank(message = "El destino no puede estar en blanco") String destination){
-        HotelConsultDTO datos = new HotelConsultDTO(dateFrom, dateTo, destination);
-        return new ResponseEntity<>(hotelService.hotelesDisponibles(datos), HttpStatus.OK);
-    }
-
-    //US 0003:
-    @PostMapping("/booking")
-    public ResponseEntity<?> reserved(@RequestBody @Valid FinalHotelReservationDTO finalHotelReservationDTO) {
-        return new ResponseEntity<>(hotelService.reserved(finalHotelReservationDTO), HttpStatus.CREATED);
-    }
-
-    //HOTELES RESERVADOS
-    @GetMapping("/reservedHotels")
-    public ResponseEntity<?> hotelsSaved() {
-        return new ResponseEntity<>(hotelService.hotelSaved(), HttpStatus.OK);
-    }
-
-    //CREATE
-    @PostMapping("/createHotel")
-    public ResponseEntity<ResponseDTO> createHotel(@RequestBody @Valid HotelDTO hotelDTO) {
-        return new ResponseEntity<>(hotelService.createHotel(hotelDTO), HttpStatus.CREATED);
-    }
-
-    //UPDATE
-    @PutMapping("/updateHotel/{id}")
-    public ResponseEntity<ResponseDTO> updateHotel(@PathVariable @NotNull Long id, @RequestBody HotelDTO hotelDTO) {
-
-        return new ResponseEntity<>(hotelService.updateHotel(id, hotelDTO), HttpStatus.OK);
-    }
-
-    //DELETE
-    @DeleteMapping("/deleteHotel/{hotelCode}")
-    public ResponseEntity<ResponseDTO> deleteHotel(@PathVariable @NotNull String hotelCode) {
-        return new ResponseEntity<>(hotelService.deleteHotel(hotelCode), HttpStatus.OK);
-    }
-}
+//@RestController
+//@RequestMapping("/api/v1")
+//@Validated
+//public class HotelController {
+//
+//    @Autowired
+//    private IHotelService hotelService;
+//
+//    //US 0001:
+//    @GetMapping("/listHotels") //
+//    public ResponseEntity<?> listarHoteles(){
+//        return new ResponseEntity<>(hotelService.listHotelsDTO(), HttpStatus.OK);
+//    }
+//
+//    //US 0002:
+//    @GetMapping("/hotels")
+//    public ResponseEntity<?> hotelesDisponibles(@RequestParam  @Future(message = "La fecha de entrada debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateFrom,
+//                                                @RequestParam @Future(message = "La fecha de salida debe ser en el futuro") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dateTo,
+//                                               @RequestParam @NotBlank(message = "El destino no puede estar en blanco") String destination){
+//        HotelConsultDTO datos = new HotelConsultDTO(dateFrom, dateTo, destination);
+//        return new ResponseEntity<>(hotelService.hotelesDisponibles(datos), HttpStatus.OK);
+//    }
+//
+//    //US 0003:
+//    @PostMapping("/booking")
+//    public ResponseEntity<?> reserved(@RequestBody @Valid FinalHotelReservationDTO finalHotelReservationDTO) {
+//        return new ResponseEntity<>(hotelService.reserved(finalHotelReservationDTO), HttpStatus.CREATED);
+//    }
+//
+//    //HOTELES RESERVADOS
+//    @GetMapping("/reservedHotels")
+//    public ResponseEntity<?> hotelsSaved() {
+//        return new ResponseEntity<>(hotelService.hotelSaved(), HttpStatus.OK);
+//    }
+//
+//    //CREATE
+//    @PostMapping("/createHotel")
+//    public ResponseEntity<ResponseDTO> createHotel(@RequestBody @Valid HotelDTO hotelDTO) {
+//        return new ResponseEntity<>(hotelService.createHotel(hotelDTO), HttpStatus.CREATED);
+//    }
+//
+//    //UPDATE
+//    @PutMapping("/updateHotel/{id}")
+//    public ResponseEntity<ResponseDTO> updateHotel(@PathVariable @NotNull Long id, @RequestBody HotelDTO hotelDTO) {
+//
+//        return new ResponseEntity<>(hotelService.updateHotel(id, hotelDTO), HttpStatus.OK);
+//    }
+//
+//    //DELETE
+//    @DeleteMapping("/deleteHotel/{hotelCode}")
+//    public ResponseEntity<ResponseDTO> deleteHotel(@PathVariable @NotNull String hotelCode) {
+//        return new ResponseEntity<>(hotelService.deleteHotel(hotelCode), HttpStatus.OK);
+//    }
+//}
 
