8e3b32f5db4fb755607b68a3595062a5ef0a9458
poupeaug <poupeaug@gmail.com>
2024-07-09 12:10:28 -0300

    validación controller hotel

--- src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/HotelController.java
@@ -4,6 +4,7 @@ import com.example.AgenciaTurismo.dto.HotelDTO;
 import com.example.AgenciaTurismo.dto.request.HotelConsultDTO;
 import com.example.AgenciaTurismo.dto.response.ResponseDTO;
 import com.example.AgenciaTurismo.service.IHotelService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/hotels")
 public class HotelController {
 
@@ -30,13 +33,13 @@ public class HotelController {
 
     //CREAR
     @PostMapping("/hotels/new")
-    public ResponseEntity<ResponseDTO> crearHotel(@RequestBody HotelDTO hotelDTO) {
+    public ResponseEntity<ResponseDTO> crearHotel(@RequestBody @Valid HotelDTO hotelDTO) {
         return new ResponseEntity<>(service.createHotel(hotelDTO), HttpStatus.CREATED);
     }
 
     //UPDATE
     @PutMapping("/hotels/edit/{id}")
-    public ResponseEntity<ResponseDTO> updateHotel(@PathVariable @NotNull Long id, @RequestBody HotelDTO hotelDTO) {
+    public ResponseEntity<ResponseDTO> updateHotel(@PathVariable @NotNull Long id, @RequestBody @Valid HotelDTO hotelDTO) {
         return new ResponseEntity<>(service.updateHotel(id, hotelDTO), HttpStatus.OK);
     }
 
