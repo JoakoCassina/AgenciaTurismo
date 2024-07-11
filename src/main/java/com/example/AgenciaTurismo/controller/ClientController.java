b82361e6b053cfb6bddbc521925ef5e2235b19c5
Juancho022 <juanma.francesconi15@gmail.com>
2024-07-11 12:06:00 -0300

    Añadiendo metodos en cliente service y controllador service para Requerimiento 4

--- src/main/java/com/example/AgenciaTurismo/controller/ClientController.java
+++ src/main/java/com/example/AgenciaTurismo/controller/ClientController.java
@@ -2,13 +2,12 @@ package com.example.AgenciaTurismo.controller;
 
 
 import com.example.AgenciaTurismo.dto.ClienteDTO;
+import com.example.AgenciaTurismo.model.Client;
 import com.example.AgenciaTurismo.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +18,18 @@ public class ClientController {
     @Autowired
     IClientService clientService;
 
-    @GetMapping("/top-3")
-    ResponseEntity<List<ClienteDTO>> topClientes(){
-        return new ResponseEntity<>(clientService.topClientes(), HttpStatus.OK);
+//    @GetMapping("/top-3")
+//    ResponseEntity<List<ClienteDTO>> topClientes(){
+//        return new ResponseEntity<>(clientService.topClientes(), HttpStatus.OK);
+//    }
+
+    @GetMapping
+    ResponseEntity<List<Client>> listarClientes(){
+        return new ResponseEntity<>(clientService.listarClientes(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userName}")
+    ResponseEntity<ClienteDTO> findByUsername(@PathVariable String userName){
+        return new ResponseEntity<>(clientService.findByUsername(userName), HttpStatus.OK);
     }
 }
